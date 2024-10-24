b43c472b24cce3bfd7c41b36c830a73d88606c38
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-25 00:51:40 +0530

    React-JS-Template

---
+++ Template-3/template3/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header3 from './Template3/Header3';
+import Section from './Template3/Section';
+import Footer from './Template3/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header3/>
+      <Section/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
