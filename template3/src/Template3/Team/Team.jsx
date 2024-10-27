81f9d6539ed10a3f0276c611fb5a098f1a4ff5f0
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-27 11:45:50 +0530

    React-Converts-Fenix

--- template3/src/Template3/Team/Team.jsx
+++ template3/src/Template3/Team/Team.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import teamData from './teamData.json'; // Adjust the path if necessary
 
 class Team extends Component {
     render() {
@@ -6,140 +7,50 @@ class Team extends Component {
             <section className="team_section layout_padding">
                 <div className="container-fluid">
                     <div className="heading_container heading_center">
-                        <h2 className>
-                            Our <span> Team</span>
+                        <h2>
+                            {teamData.heading} <span>{teamData.subheading}</span>
                         </h2>
                     </div>
                     <div className="team_container">
                         <div className="row">
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="box ">
-                                    <div className="img-box">
-                                        <img src="images/team-1.jpg" className="img1" alt />
-                                    </div>
-                                    <div className="detail-box">
-                                        <h5>
-                                            Joseph Brown
-                                        </h5>
-                                        <p>
-                                            Marketing Head
-                                        </p>
-                                    </div>
-                                    <div className="social_box">
-                                        <a href="#">
-                                            <i className="fa fa-facebook" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-twitter" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-linkedin" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-instagram" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-youtube-play" aria-hidden="true" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="box ">
-                                    <div className="img-box">
-                                        <img src="images/team-2.jpg" className="img1" alt />
-                                    </div>
-                                    <div className="detail-box">
-                                        <h5>
-                                            Nancy White
-                                        </h5>
-                                        <p>
-                                            Marketing Head
-                                        </p>
-                                    </div>
-                                    <div className="social_box">
-                                        <a href="#">
-                                            <i className="fa fa-facebook" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-twitter" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-linkedin" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-instagram" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-youtube-play" aria-hidden="true" />
-                                        </a>
+                            {teamData.description.map((member, index) => (
+                                <div className="col-lg-3 col-sm-6" key={index}>
+                                    <div className="box">
+                                        <div className="img-box">
+                                            <img src={member.image} className="img1" alt={member.name} />
+                                        </div>
+                                        <div className="detail-box">
+                                            <h5>
+                                                {member.name}
+                                            </h5>
+                                            <p>
+                                                {member.role}
+                                            </p>
+                                        </div>
+                                        <div className="social_box">
+                                            {member.links.map((link, idx) => (
+                                                <React.Fragment key={idx}>
+                                                    <a href={link.fb}>
+                                                        <i className="fa fa-facebook" aria-hidden="true" />
+                                                    </a>
+                                                    <a href={link.twitter}>
+                                                        <i className="fa fa-twitter" aria-hidden="true" />
+                                                    </a>
+                                                    <a href={link.linkedin}>
+                                                        <i className="fa fa-linkedin" aria-hidden="true" />
+                                                    </a>
+                                                    <a href={link.insta}>
+                                                        <i className="fa fa-instagram" aria-hidden="true" />
+                                                    </a>
+                                                    <a href={link.youtube}>
+                                                        <i className="fa fa-youtube-play" aria-hidden="true" />
+                                                    </a>
+                                                </React.Fragment>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="box ">
-                                    <div className="img-box">
-                                        <img src="images/team-3.jpg" className="img1" alt />
-                                    </div>
-                                    <div className="detail-box">
-                                        <h5>
-                                            Earl Martinez
-                                        </h5>
-                                        <p>
-                                            Marketing Head
-                                        </p>
-                                    </div>
-                                    <div className="social_box">
-                                        <a href="#">
-                                            <i className="fa fa-facebook" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-twitter" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-linkedin" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-instagram" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-youtube-play" aria-hidden="true" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="box ">
-                                    <div className="img-box">
-                                        <img src="images/team-4.jpg" className="img1" alt />
-                                    </div>
-                                    <div className="detail-box">
-                                        <h5>
-                                            Josephine Allard
-                                        </h5>
-                                        <p>
-                                            Marketing Head
-                                        </p>
-                                    </div>
-                                    <div className="social_box">
-                                        <a href="#">
-                                            <i className="fa fa-facebook" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-twitter" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-linkedin" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-instagram" aria-hidden="true" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fa fa-youtube-play" aria-hidden="true" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -148,4 +59,4 @@ class Team extends Component {
     }
 }
 
-export default Team
+export default Team;
