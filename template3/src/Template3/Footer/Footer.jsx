680a66f451f6eb4775096f2bc6c92213db94629f
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-27 12:21:20 +0530

    React-Converts-Finexo

--- template3/src/Template3/Footer/Footer.jsx
+++ template3/src/Template3/Footer/Footer.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import footerData from './footerData.json';
 
 class Footer extends Component {
   render() {
-    return (
-<section classname="footer_section">
-  <div classname="container">
-    <p>
-      © <span id="displayYear"> All Rights Reserved By
-        <a href="https://html.design/">Free Html Templates</a>
-      </span></p>
-  </div>
-</section>
+    const { sectionClassName, containerClassName, text, yearId, link } = footerData;
 
-      
-    )
+    return (
+      <section className={sectionClassName}>
+        <div className={containerClassName}>
+          <p>
+            {text} <span id={yearId} /> {text}
+            <a href={link.url}>{link.text}</a>
+          </p>
+        </div>
+      </section>
+    );
   }
 }
 
-export default Footer
+export default Footer;
--- template3/src/Template3/Info/Info.jsx
+++ template3/src/Template3/Info/Info.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import infoData from './infoData.json';
 
 class Info extends Component {
     render() {
@@ -6,98 +7,76 @@ class Info extends Component {
             <section className="info_section layout_padding2">
                 <div className="container">
                     <div className="row">
+                        {/* Address Section */}
                         <div className="col-md-6 col-lg-3 info_col">
                             <div className="info_contact">
-                                <h4>
-                                    Address
-                                </h4>
+                                <h4>Address</h4>
                                 <div className="contact_link_box">
-                                    <a href>
+                                    <a href="#">
                                         <i className="fa fa-map-marker" aria-hidden="true" />
-                                        <span>
-                                            Location
-                                        </span>
+                                        <span>{infoData.address.location}</span>
                                     </a>
-                                    <a href>
+                                    <a href="#">
                                         <i className="fa fa-phone" aria-hidden="true" />
-                                        <span>
-                                            Call +01 1234567890
-                                        </span>
+                                        <span>Call {infoData.address.phone}</span>
                                     </a>
-                                    <a href>
+                                    <a href="#">
                                         <i className="fa fa-envelope" aria-hidden="true" />
-                                        <span>
-                                            demo@gmail.com
-                                        </span>
+                                        <span>{infoData.address.email}</span>
                                     </a>
                                 </div>
                             </div>
                             <div className="info_social">
-                                <a href>
+                                <a href="#">
                                     <i className="fa fa-facebook" aria-hidden="true" />
                                 </a>
-                                <a href>
+                                <a href="#">
                                     <i className="fa fa-twitter" aria-hidden="true" />
                                 </a>
-                                <a href>
+                                <a href="#">
                                     <i className="fa fa-linkedin" aria-hidden="true" />
                                 </a>
-                                <a href>
+                                <a href="#">
                                     <i className="fa fa-instagram" aria-hidden="true" />
                                 </a>
                             </div>
                         </div>
+
+                        {/* Info Section */}
                         <div className="col-md-6 col-lg-3 info_col">
                             <div className="info_detail">
-                                <h4>
-                                    Info
-                                </h4>
-                                <p>
-                                    necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful
-                                </p>
+                                <h4>{infoData.info.title}</h4>
+                                <p>{infoData.info.description}</p>
                             </div>
                         </div>
+
+                        {/* Links Section */}
                         <div className="col-md-6 col-lg-2 mx-auto info_col">
                             <div className="info_link_box">
-                                <h4>
-                                    Links
-                                </h4>
+                                <h4>Links</h4>
                                 <div className="info_links">
-                                    <a className="active" href="index.html">
-                                        Home
-                                    </a>
-                                    <a className href="about.html">
-                                        About
-                                    </a>
-                                    <a className href="service.html">
-                                        Services
-                                    </a>
-                                    <a className href="why.html">
-                                        Why Us
-                                    </a>
-                                    <a className href="team.html">
-                                        Team
-                                    </a>
+                                    {infoData.links.map((link, index) => (
+                                        <a key={index} href={link.url} className={link.isActive ? "active" : ""}>
+                                            {link.name}
+                                        </a>
+                                    ))}
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-3 info_col ">
-                            <h4>
-                                Subscribe
-                            </h4>
+
+                        {/* Subscribe Section */}
+                        <div className="col-md-6 col-lg-3 info_col">
+                            <h4>Subscribe</h4>
                             <form action="#">
                                 <input type="text" placeholder="Enter email" />
-                                <button type="submit">
-                                    Subscribe
-                                </button>
+                                <button type="submit">Subscribe</button>
                             </form>
                         </div>
                     </div>
                 </div>
             </section>
-
-        )
+        );
     }
 }
 
-export default Info
+export default Info;
